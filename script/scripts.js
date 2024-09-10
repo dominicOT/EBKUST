d616ccac004e43fb7c64c8876b96d74dd7a26114
Dominic Oladapo-Tonade <dominicoladapotonade@gmail.com>
2024-09-10 07:24:27 +0100

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -18,33 +18,41 @@ document.getElementById('registrationForm').addEventListener('submit', submitFor
 function submitForm(event) {
   event.preventDefault();
 
-  var id_num = document.getElementById('id_num').value.trim();
-  var firstName = document.getElementById('firstName').value.trim();
-  var middleName = document.getElementById('middleName').value.trim();
-  var lastName = document.getElementById('lastName').value.trim();
-  var phone = document.getElementById('phone').value.trim();
-  var email = document.getElementById('email').value.trim();
-  var address = document.getElementById('address').value.trim();
-  var program = document.getElementById('program').value.trim();
-  var department = document.getElementById('department').value;
-  var year = document.getElementById('year').value;
-  var semester = document.getElementById('semester').value;
-  var fees = document.getElementById('fees').value;
-  var parentFirstName = document.getElementById('parentFirstName').value.trim();
-  var parentLastName = document.getElementById('parentLastName').value.trim();
-  var parentPhone = document.getElementById('parentPhone').value.trim();
-  var parentEmail = document.getElementById('parentEmail').value.trim();
+  var studentInfo = {
+      id_num: document.getElementById('id_num').value.trim(),
+      firstName: document.getElementById('firstName').value.trim(),
+      middleName: document.getElementById('middleName').value.trim(),
+      lastName: document.getElementById('lastName').value.trim(),
+      phone: document.getElementById('phone').value.trim(),
+      email: document.getElementById('email').value.trim(),
+      address: document.getElementById('address').value.trim(),
+  };
 
-  saveMessages(id_num, firstName, middleName, lastName, phone, email, address, department, program, year, semester, fees, parentFirstName, parentLastName, parentPhone, parentEmail);
+  var academicInfo = {
+      department: document.getElementById('department').value,
+      program: document.getElementById('program').value.trim(),
+      year: document.getElementById('year').value,
+      semester: document.getElementById('semester').value,
+      feesStatus: document.getElementById('fees').value
+  };
+
+  var parentInfo = {
+      firstName: document.getElementById('parentFirstName').value.trim(),
+      lastName: document.getElementById('parentLastName').value.trim(),
+      phone: document.getElementById('parentPhone').value.trim(),
+      email: document.getElementById('parentEmail').value.trim()
+  };
+
+
+  saveMessages(studentInfo, academicInfo, parentInfo);
+
 
   document.querySelector('.alert').style.display = 'block';
-
   setTimeout(() => {
       document.querySelector('.alert').style.display = 'none';
   }, 5000);
 
   redirectToSuccessPage();
-
   document.getElementById('registrationForm').reset();
 }
 
@@ -52,28 +60,11 @@ function redirectToSuccessPage() {
   window.location.href = "success.html";
 }
 
-const saveMessages = (id_num, firstName, middleName, lastName, phone, email, address, department, program, year, semester, fees, parentFirstName, parentLastName, parentPhone, parentEmail) => {
+const saveMessages = (studentInfo, academicInfo, parentInfo) => {
   var newRegDB = regDB.push();
   newRegDB.set({
-      id_num: id_num,
-      firstName: firstName,
-      middleName: middleName,
-      lastName: lastName,
-      phone: phone,
-      email: email,
-      address: address,
-      department: department,
-      program: program,
-      year: year,
-      semester: semester,
-      fees: fees,
-      parentFirstName: parentFirstName,
-      parentLastName: parentLastName,
-      parentPhone: parentPhone,
-      parentEmail: parentEmail
+      studentInfo: studentInfo,
+      academicInfo: academicInfo,
+      parentInfo: parentInfo
   });
-};
-
-const getElementValue = (id) => {
-  return document.getElementById(id).value;
 };
